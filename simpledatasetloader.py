761a31843955d3793abe09c3163190cface7dd55
GDavisIV <gdavisiv@gmail.com>
2020-11-22 22:21:28 -0500

    Continuing to build out datasetloader

--- simpledatasetloader.py
+++ simpledatasetloader.py
@@ -4,8 +4,12 @@ import cv2
 import os
 
 class SimpleDatasetLoader:
+    #This defines the constructor where we can pass in a list of image preprocessors , that can we sequentally
+    #applied to a specific image that was inputted
     def __init__(self, preprocessors=None):
         #stores the image preprocessor
         self.preprocessors = preprocessors
         
-        #
+        #if there is no preprocessors, initialize it as an empty list
+        if self.preprocessors is None:
+            self.preprocessors = []
