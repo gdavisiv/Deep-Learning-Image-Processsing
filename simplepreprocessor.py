67d9ec7efcb0a6d35cd42e5a2e0457fcf7808c11
GDavisIV <gdavisiv@gmail.com>
2020-11-22 22:06:35 -0500

    Completed creating the simple-preprocessor

--- simplepreprocessor.py
+++ simplepreprocessor.py
@@ -1,2 +1,15 @@
 #import the necessary packages
 import cv2
+
+class SimplePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER.AREA):
+        #stores the targe image width, height, and interpolation
+        #this method is used when resizing
+        self.width = width
+        self.height = height
+        self.inter = inter
+        
+    #The input image that we want to process    
+    def preprocess(self, image):
+        #resize the image to a fixed size, ignore the aspect ratio and return  to the calling function
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
